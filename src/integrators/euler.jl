b04a23be43565bccb0630b19a17119ddbf17d540
Stefan Ehin <stefanehin4@gmail.com>
2026-01-01 19:16:56 +0200

    Implement ForwardEuler

--- src/integrators/euler.jl
+++ src/integrators/euler.jl
@@ -1,5 +1,20 @@
 struct ForwardEuler <: AbstractODEAlgorithm end
 
-function solve(prob::AbstractODEProblem, alg::ForwardEuler)
-    
+function solve(prob::AbstractODEProblem, ::ForwardEuler; dt)
+    println("Works?")
+
+    f = get_f(prob)
+    u = copy(get_u0(prob))
+    t₀, t₁ = get_tspan(prob)
+    p = get_p(prob)
+
+    ts = t₀:dt:t₁
+    us = [copy(u)]
+
+    for t in ts[1:end-1]
+        u += dt * f(u, p, t)
+        push!(us, copy(u))
+    end
+
+    return (t=ts, u=us)
 end
