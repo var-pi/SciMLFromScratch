5f490c86e433f74492315312f5e9bd37c0a6a379
Stefan Ehin <stefanehin4@gmail.com>
2026-01-07 08:31:48 +0200

    Extract Newton solver

--- src/integrators/euler.jl
+++ src/integrators/euler.jl
@@ -5,8 +5,43 @@ struct BackwardEuler <: AbstractODEAlgorithm end
 
 @inline step(::ForwardEuler, f, u, p, t, dt) = u + dt * f(u, p, t)
 
-@inline function step(::BackwardEuler, f, u, p, t, dt; df)
+@inline function step(::BackwardEuler, f, u, p, t, dt; df, alg = Newton())
     g(x) = x - u - dt * f(x, p, t + dt)
     dg(u_next) = I - dt * df(u_next, p, t + dt)
-    newton_solve(g, dg, u)
+
+    prob = NonlinearProblem(g, u)
+    solve(prob, alg; df = dg)
+end
+
+# --------------------------------
+
+struct NonlinearProblem{F,U}
+    f::F
+    u0::U
+end
+
+abstract type AbstractNonlinearAlgorithm end
+
+struct Newton <: AbstractNonlinearAlgorithm end
+
+function solve(prob::NonlinearProblem, alg::AbstractNonlinearAlgorithm; df)
+    error("Nonlinear solve not implemented for $(typeof(alg))")
+end
+
+function solve(prob::NonlinearProblem, ::Newton; df)
+    f  = prob.f
+    u0 = prob.u0
+
+    tol = 1e-8
+    maxiter=50
+
+    u = u0
+    tol² = tol^2
+    for _ in 1:maxiter
+        Δ = df(u) \ f(u)
+        u -= Δ
+        Δ⋅Δ < tol² && break
+    end
+
+    u
 end
