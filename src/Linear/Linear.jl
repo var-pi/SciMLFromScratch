a060515c70e242b65f42a2bdcab14a5244feddb9
Stefan Ehin <stefanehin4@gmail.com>
2026-01-15 15:50:50 +0200

    Extract "solve" and "perform_step" from ODE

--- src/Linear/Linear.jl
+++ src/Linear/Linear.jl
@@ -2,23 +2,29 @@ using LinearAlgebra: norm
 
 abstract type LAlg <: AbstractSciMLAlgorithm end
 
-@kwdef mutable struct LinearState{U,V}
-    u::U
-    r::V
-    iter::Int = 0
-    retcode::ReturnCode = Default
+let @kwdef mutable struct LinearState{U,V}
+        u::U
+        r::V
+        iter::Int = 0
+        retcode::ReturnCode = Default
+    end
+
+    init((; A, b, u0)::AbstractLinearProblem) = LinearState(; u = copy(u0), r = b .- A(u0))
+
+    step_condition(
+        (; retcode, iter)::LinearState,
+        ::AbstractLinearProblem,
+        (; maxiter)::LAlg,
+    ) = retcode == Default && iter < maxiter
+
+    after_step!((; u, r)::LinearState, (; A, b)::AbstractLinearProblem, ::LAlg) =
+        r .= b .- A(u)
+
+    success_condition((; r)::LinearState, (; atol)::LAlg) = norm(r) < atol
+
+    finalize(state::LinearState) = LinearSolution(state), LinearDiagnostics(state)
+
+    include("richardson.jl")
+    export LAlg, Richardson
+
 end
-
-init((; A, b, u0)::AbstractLinearProblem) = LinearState(; u = copy(u0), r = b .- A(u0))
-
-step_condition((; retcode, iter)::LinearState, ::AbstractLinearProblem, (; maxiter)::LAlg) =
-    retcode == Default && iter < maxiter
-
-after_step!((; u, r)::LinearState, (; A, b)::AbstractLinearProblem, ::LAlg) = r .= b .- A(u)
-
-success_condition((; r)::LinearState, (; atol)::LAlg) = norm(r) < atol
-
-finalize(state::LinearState) = LinearSolution(state), LinearDiagnostics(state)
-
-include("richardson.jl")
-export LAlg, Richardson
